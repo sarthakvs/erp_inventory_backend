cf352ef8844cc49b019563fb4197a6dd52ddcde1
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2024-10-24 06:39:37 +0530

    first

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 
 // Setting Up connection
 mongoose.set('strictQuery', false);
-const dbURL = process.env.DB_URL || "mongodb://localhost:27017/iStockDB"
+const dbURL = process.env.DB_URL;
 
 mongoose.connect(dbURL, {useNewUrlParser: true}).then(()=>{
     console.log("Mongo Connected");
