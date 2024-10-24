264b9793a7bb8bcc43770847eb5532b8c4e96a9f
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2024-10-24 06:35:58 +0530

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose")
+const cors = require("cors")
+const app = express();
+const cookieParser = require("cookie-parser");
+
+// Setting Up connection
+mongoose.set('strictQuery', false);
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/iStockDB"
+
+mongoose.connect(dbURL, {useNewUrlParser: true}).then(()=>{
+    console.log("Mongo Connected");
+}).catch(err=>{
+    console.log("OH error");
+    console.log(err);
+});
+app.use(cookieParser());
+app.use(cors(
+    {
+        origin: ["http://localhost:5001"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    }
+));
+app.get("/", (req,res)=>{
+    res.send("Working");
+})
+app.use(express.json())
+// Routes
+const authRoutes = require("./routes/auth")
+const itemRoutes = require("./routes/item")
+const entryRoutes = require("./routes/entry")
+app.use("/api/auth", authRoutes);
+app.use("/api/item", itemRoutes);
+app.use("/api/entry", entryRoutes);
+
+const port = 5000;
+
+app.listen(port, ()=>{
+    console.log("Listening on ", {port});
+})
